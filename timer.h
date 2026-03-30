b6ab4727f0a51b8cb01349e40de1d0019d622ab3
Juliette Van Poulle <130056784+juliettevp@users.noreply.github.com>
2026-03-30 07:18:44 +0200

    Enhance timer.h with function comments and declarations
    Added comments for timer functions and updated function declarations.

--- timer.h
+++ timer.h
@@ -1,8 +1,6 @@
 /*
  * timer.h
- *
- *  Created on: 19 mars 2026
- *      Author: mi11p016
+ * Configuration et l'utilisation du timer TIM2
  */
 
 #ifndef TIMER_H_
@@ -10,13 +8,16 @@
 
 #include <stdint.h>
 
-void tim2_init(uint32_t psc, uint32_t arr);
-int tim2_update_pending(void);
-void tim2_clear_update(void);
+/* Méthode 1 : Timer sans interruption */
 
-void tim2_init(uint32_t psc, uint32_t arr);
-void tim2_enable_irq(void);
-void tim2_irq_handler(void);
+void tim2_init(uint32_t psc, uint32_t arr); // Initialise TIM2
+int tim2_update_pending(void); // Test le flag d'update
+void tim2_clear_update(void); // Efface le flag d'update
+
+/* Méthode 2 : Timer avec interruption */
+
+void tim2_enable_irq(void); // Active les interruptions TIM2
+void tim2_irq_handler(void); // Handler d'interruption TIM2
 
 #endif /* TIMER_H_ */
 
